cb7f34b10c29f8a6f810b762055f7c0e033ec946
Eduardo Squetini <163067276+eduardo-squetini@users.noreply.github.com>
2024-03-26 00:57:38 -0300

    Update while.java

--- while.java
+++ while.java
@@ -1,18 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package aula05;
 
-/**
- *
- * @author Eduardo
- */
 public class While {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         
         //while: enquanto for verdadeiro repete o comando. 
