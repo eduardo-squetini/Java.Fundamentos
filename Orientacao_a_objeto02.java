3b6e518d22986919bc5777d41098a0a7d359f2f1
Eduardo Squetini <163067276+eduardo-squetini@users.noreply.github.com>
2024-03-26 00:53:21 -0300

    Update Orientacao_a_objeto02.java

--- Orientacao_a_objeto02.java
+++ Orientacao_a_objeto02.java
@@ -1,21 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package orientacao_a_objeto02;
 
-/**
- *
- * @author Eduardo
- */
 public class POO_02 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-         //Armazenar a nota a1, a2, a3 de 10 alunos, juntamente com o  nome do aluno usando POO
+      
+         /*Armazenar a nota a1, a2, a3 de 10 alunos, juntamente com o  nome do aluno usando POO*/
         
      
     }
