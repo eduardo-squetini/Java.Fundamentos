07e20ac181c223662deb9d8f058134a9245cca4f
Eduardo Squetini <163067276+eduardo-squetini@users.noreply.github.com>
2024-03-26 00:53:55 -0300

    Update Orientacao_a_objeto03.java

--- Orientacao_a_objeto03.java
+++ Orientacao_a_objeto03.java
@@ -1,18 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package orientacao_a_objeto03;
 
-/**
- *
- * @author Eduardo
- */
 public class POO_03 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         
        /* Teste tst = new Teste();
@@ -38,11 +27,7 @@ public class POO_03 {
        System.out.println("\n" + b.getArea());
        System.out.println(b.getPerimetro());
        System.out.println(b.getDefinicao());
-        
-        
-        
-        
-        // TODO code application logic here
+
     }
     
 }
