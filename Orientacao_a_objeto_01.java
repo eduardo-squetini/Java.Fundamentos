9bdc7b8136fe38a8fd4b0c8bf31693ce1d836571
Eduardo Squetini <163067276+eduardo-squetini@users.noreply.github.com>
2024-03-26 00:54:17 -0300

    Update Orientacao_a_objeto_01.java

--- Orientacao_a_objeto_01.java
+++ Orientacao_a_objeto_01.java
@@ -1,18 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package orientacao_a_objeto_01;
 
-/**
- *
- * @author Eduardo
- */
 public class POO_01 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
         
