062f655a0bdde760a2a44175530dd86cc1618046
Eduardo Squetini <163067276+eduardo-squetini@users.noreply.github.com>
2024-03-26 00:52:06 -0300

    Update Formas.java

--- Formas.java
+++ Formas.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package orientacao_a_objeto03;
 
-/**
- *
- * @author Eduardo
- */
 public class Formas {
     
     private double area;
